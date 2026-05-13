c89c1542666fa4886992b26ffe77646bc149d4c5
baneetkumar-07 <78535799+baneetkumar-07@users.noreply.github.com>
2026-05-13 12:27:30 +0530

    Time: 0 ms (100%), Space: 55.4 MB (83.17%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,13 +3,7 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    // power of two means divide by 2 until remiander is 1 (recursion)
-    // break when newNum < n
-    if(n <= 0) return false;
-    if(n === 1) return true;
-
-    if(n % 2 !== 0) return false;
-
-    return isPowerOfTwo(n/2)
+    // using bitwise operator
+    return n > 0 && (n & (n-1)) === 0;
     
 };
