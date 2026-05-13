979f505082edddd71344568f01dfea1acd255bbf
baneetkumar-07 <78535799+baneetkumar-07@users.noreply.github.com>
2026-05-14 00:17:48 +0530

    Time: 0 ms (100%), Space: 56.4 MB (92.76%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    //brute force
+
+    let k = 1;
+
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] !== nums[k - 1]){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    
+   return k
+};
