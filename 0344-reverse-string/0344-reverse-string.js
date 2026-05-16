ed5cd6c765133e88b7ca8d164464ead9717d38a4
baneetkumar-07 <78535799+baneetkumar-07@users.noreply.github.com>
2026-05-16 20:49:16 +0530

    Time: 3 ms (17.31%), Space: 61.7 MB (43.17%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -10,11 +10,9 @@ var reverseString = function(s) {
     let j = s.length - 1;
 
     while( i < j){
-        let temp = s[j];
-        s[j] = s[i];
-        s[i] = temp;
-        i++;
-        j--;
+       [s[i],s[j]] = [s[j], s[i]];
+       i++;
+       j--;
     }
     return s;
 };
